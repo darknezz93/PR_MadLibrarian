437c243a9720684b3e85b90e0ee8c0a163d7e122
Marek Derkowski <inf110257@lab-sec-14.cs.put.poznan.pl>
2015-06-16 23:32:53 +0200

    smiga nowa wersja

--- LibGlob/Librarian2.cpp
+++ LibGlob/Librarian2.cpp
@@ -6,9 +6,10 @@
 #include <ctime>
 #include <cstdlib>
 #include <unistd.h>
-#include "MPC.cpp"
+
 #define ROOT 0
 #define MSG_TAG 100
+#define NUMB_OF_LIBR 5
 
 using namespace std;
 
@@ -17,19 +18,19 @@ using namespace std;
 	int customersCount;
 	int msg[3];
 	list<int> processesIds;
-	int priorities[]; //[0] brak zgody [1] wygrana walka [2] zezwolenie
-    list<MPC> freeMPCs;
+	//int priorities[NUMB_OF_LIBR]; //[0] brak zgody [1] wygrana walka [2] zezwolenie
+    	//list<MPC> freeMPCs;
 	bool czyMogeWejsc;
 
-	void create(int id, int size);	
-	void sendRequests();
-	void canEnter();
-	void waitForAnswears();
-	void readAnswer(int id, int numOfReaders, int answer);
-	void accessMPC();
+	//void create(int id, int size, int priorities);	
+	//void sendRequests();
+	//void canEnter();
+	//void waitForAnswears();
+	void readAnswer(int id, int numOfReaders, int answer, int prio[]);
+	//void accessMPC(int prio[]);
+	
 
-
-void create(int id, int size){
+void create(int id, int size, int priorities[]){
 	srand(time(0));
 	tid = id;
 	size = size;
@@ -58,8 +59,9 @@ void sendRequests() {
 	}
 }
 
-void canEnter() {
+void canEnter(int priorities[]) {
 	//sprawdzenie tablicy priorytetów
+	//printf("canEnter\n");
 	czyMogeWejsc = true;
 	for (int i = 0; i<size; i++){
 		if (priorities[i] == 0){
@@ -69,16 +71,16 @@ void canEnter() {
 	}
 }
 
-void waitForAnswears() {
+void waitForAnswears(int prio[]) {
 	//oczekiwanie odpowiedzi
 	for (int i = 1; i<size; i++){ //oczekuje na size-1 odpowiedzi
 		MPI_Status status;
 		MPI_Recv(msg, 3, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-		readAnswer(msg[0], msg[1], msg[2]);
+		readAnswer(msg[0], msg[1], msg[2], prio);
 	}
 }
 
-void accessMPC() {
+void accessMPC(int priorities[]) {
 	//dostêp do MPC
 	if (czyMogeWejsc){
 		printf("Weszlem(%d)--------------------------------------\n", tid);
@@ -95,9 +97,9 @@ void accessMPC() {
 
 
 
-void readAnswer(int id, int numbOfReaders, int answer){ 
+void readAnswer(int id, int numbOfReaders, int answer, int priorities[]){ 
 	//printf("Metoda ReadAnswer()\n");
-	printf("(%d | %d)Odpowiedz od: %d | %d | %d\n", tid, customersCount, id, numbOfReaders, answer);
+	//printf("(%d | %d)Odpowiedz od: %d | %d | %d\n", tid, customersCount, id, numbOfReaders, answer);
 	if (answer == 100){ // 100 - kod dla odpowiedzi "agree"
 		priorities[id] = 2; //2 - wartosc dla odpowiedzi "agree"
 		return;
@@ -110,7 +112,7 @@ void readAnswer(int id, int numbOfReaders, int answer){
 		priorities[id] = 1; //1 - zezwolenie (wygrana walka)
 		return;
 	}
-	if (numbOfReaders == customersCount and tid < id)
+	if (numbOfReaders == customersCount and tid > id)
 	{
 		priorities[id] = 1;
 		return;
@@ -124,22 +126,25 @@ void readAnswer(int id, int numbOfReaders, int answer){
 
 int main(int argc, char **argv)
 {
-	int tid, size, len;
+	int  len;
 	char processor[100];
+
 	//MPI_Status status;
 
 	MPI::Init(argc, argv);
 	size = MPI::COMM_WORLD.Get_size();
 	tid = MPI::COMM_WORLD.Get_rank();
 	MPI::Get_processor_name(processor, len);
-
-	create(tid, size);
-	printf("Hello! My name is %s (%d of %d)\n", processor, tid, size);
+	
+	int priorities[size];
+	create(tid, size, priorities);
+	//printf("Hello! My name is %s (%d of %d)\n", processor, tid, size);
 	
 	sendRequests();
-	waitForAnswears();
-	canEnter();
-	accessMPC();
+	waitForAnswears(priorities);
+	//for(int i=0; i<size; i++){ printf("%d ", priorities[i]); printf("[%d]\n", tid); }
+	canEnter(priorities);
+	accessMPC(priorities);
 	
 
 	MPI::Finalize();
